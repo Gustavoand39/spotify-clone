4b449db02d0e46d6ee8509645e447b4e17dd90c3
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-07 17:07:05 -0600

    feat: :sparkles: Add Svelte dependecy and Greetings component

---
+++ src/components/Greetings.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+
+  const messages = {
+    mañana: "¡Buenos días!",
+    tarde: "¡Buenas tardes!",
+    noche: "¡Buenas noches!",
+    madrugada: "¡Es hora de dormir!",
+  };
+
+  if (currentHour >= 6 && currentHour < 12) {
+    greeting = messages.mañana;
+  } else if (currentHour >= 12 && currentHour < 19) {
+    greeting = messages.tarde;
+  } else if (currentHour >= 19 && currentHour < 23) {
+    greeting = messages.noche;
+  } else {
+    greeting = messages.madrugada;
+  }
+</script>
+
+<h1 class="text-3xl font-bold text-white">{greeting}</h1>

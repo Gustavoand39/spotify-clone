49e9256be729e4b027481d1e652bce63c446ffc9
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-06-08 13:59:28 -0600

    feat: :sparkles: Add volume control to Player component

--- src/store/playerStore.ts
+++ src/store/playerStore.ts
@@ -4,17 +4,21 @@ import type { Playlist, Song } from "@/lib/data";
 interface PlayerStore {
   isPlaying: boolean;
   currentSong: { playlist: Playlist | null; song: Song | null; songs: Song[] };
+  volume: number;
   setIsPlaying: (isPlaying: boolean) => void;
   setCurrentSong: (currentSong: {
     playlist: Playlist | null;
     song: Song | null;
     songs: Song[];
   }) => void;
+  setVolume: (volume: number) => void;
 }
 
 export const usePlayerStore = create<PlayerStore>((set) => ({
   isPlaying: false,
   currentSong: { playlist: null, song: null, songs: [] },
+  volume: 1,
   setIsPlaying: (isPlaying: boolean) => set({ isPlaying }),
   setCurrentSong: (currentSong: any) => set({ currentSong }),
+  setVolume: (volume: number) => set({ volume }),
 }));
